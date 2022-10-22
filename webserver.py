7084ab3991dcf9b050e7776e37b4c9c83889c166
jerichoma <33026511+jerichoma@users.noreply.github.com>
2022-10-22 15:52:22 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -6,7 +6,7 @@ class helloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('content-type', 'text/html') 
         self.end_headers() 
-        self.wfile.write('Hello Jericho!'.encode()) 
+        self.wfile.write('Hello Test!'.encode()) 
 
 
 
